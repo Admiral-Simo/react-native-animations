b1e4e6a7bc3ff1e1ca3ae4168204866693fc5314
Admiral-Simo <admiralsimo16@gmail.com>
2023-04-11 20:11:53 +0000

    upfate

--- pages/HomeScreen.tsx
+++ pages/HomeScreen.tsx
@@ -1,18 +1,47 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, TextInput, TouchableOpacity, StatusBar} from 'react-native';
 import Chrono from '../components/Chrono';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import moment from 'moment';
 
 const HomeScreen = () => {
+  const [isStarted, setIsStarted] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      isStarted && setCurrentTime(prev => prev + 1000);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [isStarted]);
+
+  const reset = () => {
+    setCurrentTime(0);
+    setIsStarted(false);
+  };
+
   return (
     <View className="flex-1 bg-black">
       <View className="items-center justify-center mt-36">
         <TouchableOpacity
+          onPress={() => setIsStarted(prev => !prev)}
           activeOpacity={0.7}
           className="items-center justify-center bg-white rounded-full w-44 h-44">
-          <Text className="text-5xl text-black">RUN</Text>
+          {currentTime === 0 ? (
+            <Text className="text-5xl text-black font-rubikBold">RUN</Text>
+          ) : (
+            <Text className="text-5xl text-black font-rubikBold">Split</Text>
+          )}
+          <TouchableOpacity
+            onPress={reset}
+            className="absolute bottom-0 right-0 bg-purple-500 rounded-full">
+            <MaterialCommunityIcons name="restart" size={32} />
+          </TouchableOpacity>
         </TouchableOpacity>
       </View>
-      <Text className='mt-10 text-center text-white text-7xl'>00:00:00</Text>
+      <Text className="mt-10 text-center text-white text-7xl font-rubikLight">
+        {moment(currentTime).format('mm:ss')}
+      </Text>
       <StatusBar backgroundColor="black" barStyle="light-content" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,34 @@
 module.exports = {
   content: [
     './App.{js,jsx,ts,tsx}',
-    './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubikLight: 'Rubik-Light',
+        rubikRegular: 'Rubik-Regular',
+        rubikBold: 'Rubik-Bold',
+        rubikSemiBold: 'Rubik-SemiBold',
+        rubikBlack: 'Rubik-Black',
+        rubikItalic: 'Rubik-Italic',
+        rubikMedium: 'Rubik-Medium',
+        rubikExtraBold: 'Rubik-ExtraBold',
+        rubikExtraBoldItalic: 'Rubik-ExtraBoldItalic',
+        rubikLightItalic: 'Rubik-LightItalic',
+        rubikMediumItalic: 'Rubik-MediumItalic',
+        rubikSemiBoldItalic: 'Rubik-SemiBoldItalic',
+        rubikBoldItalic: 'Rubik-BoldItalic',
+        rubikBlackItalic: 'Rubik-BlackItalic',
+      },
+      colors: {
+        primary: '#F77658',
+        secondary: '#30244F',
+      },
+    },
   },
   plugins: [],
 };
+
+// tailwind.config.js
